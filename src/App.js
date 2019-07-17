489c846a5a99bcde9b13a72df2a47d172ae08615
Xiaoping-Li <narci2008@gmail.com>
2019-07-17 15:37:02 -0700

    App page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <p>App page</p>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navigation />
+        <Route path='/' exact component={Home} />
+        <Route path='/about' exact component={About} />
+        <Route path='/projects' exact component={Projects} />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
