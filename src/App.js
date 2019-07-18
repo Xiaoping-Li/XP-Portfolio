a12cac495897184415de6adc7d53a2538935b9ff
Xiaoping-Li <narci2008@gmail.com>
2019-07-18 09:58:54 -0700

    Modify intro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-// import { BrowserRouter, Route } from 'react-router-dom';
-// import Navigation from './components/Navigation';
-// import Home from './components/Home';
-// import About from './components/About';
-// import Projects from './components/Projects';
-// import Footer from './components/Footer';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Footer from './components/Footer';
 
 import './App.css';
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <div className="App">
-    //     <Navigation />
-    //     <Route path='/' exact component={Home} />
-    //     <Route path='/about' exact component={About} />
-    //     <Route path='/projects' exact component={Projects} />
-    //     <Footer />
-    //   </div>
-    // </BrowserRouter>
-    <div className="App">Hello world</div>
+    <BrowserRouter>
+      <div className="App">
+        <Navigation />
+        <Route path='/' exact component={Home} />
+        <Route path='/about' exact component={About} />
+        <Route path='/projects' exact component={Projects} />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className="home">
       <div className="intro-container">
-        <p className="intro">Hi, I am <span style={{color: '#c600eb'}}>XP</span><br />A Full-stack <span style={{backgroundColor: '#39ff14', color: '#fff',}}>Web</span> | <span style={{color: '#fe019a'}}>Mobile</span> Developer<br />Passionate about <span style={{color: '#1000e5'}}>Solving</span> problems, Learning new skills <span style={{backgroundColor: '#efff1a', color: '#1000e5', padding: '0 0.3em', borderRadius: '0.5em'}}>&</span> Keeping things Easy <span style={{color: '#ff073a'}}>and</span> Pretty.</p>
+        <p className="intro">Hi, I am <span style={{backgroundColor: '#c600eb', color: '#fff', padding: '0 0.3em', borderRadius: '0.5em', textShadow: '2px 2px #411749', boxShadow: '2px 2px #411749' }}>XP</span><br />A Full-stack <span style={{color: '#39ff14'}}>Web</span> | <span style={{color: '#fe019a'}}>Mobile</span> Developer<br />Passionate about <span style={{color: '#1000e5'}}>Solving</span> problems, Learning new skills <span style={{backgroundColor: '#efff1a', color: '#1000e5', padding: '0 0.3em', borderRadius: '0.5em'}}>&</span> Keeping things Easy <span style={{color: '#ff073a'}}>and</span> Pretty.</p>
       </div>
       <div className="arr-container">
         <img className="arr_down bounce" src={down} alt="arrow down" />
