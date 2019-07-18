a9f743f16a80f7f38dc2dfc82a0f7af4e53aeb95
Xiaoping-Li <narci2008@gmail.com>
2019-07-18 10:09:10 -0700

    test gh-pages

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,18 @@ import down from '../images/arr_down.png';
 function Home() {
   return (
     <div className="home">
+      {/* Personal Introduction */}
       <div className="intro-container">
         <p className="intro">Hi, I am <span style={{backgroundColor: '#c600eb', color: '#fff', padding: '0 0.3em', borderRadius: '0.5em', textShadow: '2px 2px #411749', boxShadow: '2px 2px #411749' }}>XP</span><br />A Full-stack <span style={{color: '#39ff14'}}>Web</span> | <span style={{color: '#fe019a'}}>Mobile</span> Developer<br />Passionate about <span style={{color: '#1000e5'}}>Solving</span> problems, Learning new skills <span style={{backgroundColor: '#efff1a', color: '#1000e5', padding: '0 0.3em', borderRadius: '0.5em'}}>&</span> Keeping things Easy <span style={{color: '#ff073a'}}>and</span> Pretty.</p>
       </div>
+      {/* Pointing down arrow */}
       <div className="arr-container">
         <img className="arr_down bounce" src={down} alt="arrow down" />
+      </div>
+      {/* About */}
+      <div className="home-about">
+        
+
       </div>
     </div>
     
