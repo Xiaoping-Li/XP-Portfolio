60d84de19c6937fd02e126a966741265a8fc722e
Xiaoping-Li <narci2008@gmail.com>
2019-07-19 10:44:04 -0700

    data projects

---
+++ src/components/HomeProjectsList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Projects } from '../data/Projects';
+
+
+function HomeProjectsList() {
+  return (
+    <div>
+      HomeProjectsList
+    </div>
+  );
+}
+
+export default HomeProjectsList;
--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,6 +1,9 @@
 export const projects = [
   {
-    img: ['', ''],
+    img: [
+      require('../images/shopping.png'), 
+      ''
+    ],
     title: 'Pets e-Shopping',
     descShort: 'React Native e-Commerce Project',
     frontEnd: 'React Native, Expo, React Navigation, Mobx, Axios, stripe-client',
@@ -11,7 +14,10 @@ export const projects = [
     live: '',
   },
   {
-    img: ['', ''],
+    img: [
+      require('../images/instagram.png'), 
+      ''
+    ],
     title: 'Instagram Clone',
     descShort: 'React Native Social Media Project',
     frontEnd: 'React Native, Expo, React Navigation, Mobx, Axios, React-native-elements',
@@ -22,34 +28,43 @@ export const projects = [
     live: '',
   },
   {
-    img: ['', ''],
+    img: [
+      require('../images/howdiditgo.jpg'), 
+      ''
+    ],
     title: 'How_did_it_go',
     descShort: 'Team Project by using React',
     frontEnd: 'React, JavaScript, React-Bootstrap',
     backEnd: 'Express, Express-session, bcrypt, JWT, MySQL, Heroku',
-    desc: '...',
+    desc: 'Help small businesses request reviews from customers through text messages. Business owner could manage the company states, and send review request through Twilio. The business URLs in the request messages could be tracked by Bitly API, and owners could check the states of these URL links. For example, how many times the URL was clicked, etc.',
     github: 'https://github.com/Lambda-School-Labs/how_did_it_go',
     youtube: '',
     live: 'https://lambda-labs-how-did-it-go.herokuapp.com/',
   },
   {
-    img: ['', ''],
+    img: [
+      require('../images/shopping.png'), 
+      ''
+    ],
     title: 'Patient Files Manager',
     descShort: 'First Personal Full-stack React Project',
     frontEnd: 'React, Redux, JavaScript, React-Bootstrap',
     backEnd: 'Express, Express-session, bcrypt, JWT, MySQL, Heroku',
-    desc: '...',
+    desc: "My proudest part is my design of patient forms, and how the forms display records. Each form has multiple fields, and these fields will hold records. I designed separate Forms table, Fields table, and Records table. Because the format and content of a form will change frequently per protocols, so this design will make it easier and cheaper to modify the forms, only need to insert rolls to Forms and Fields tables. From the Forms Dropdown list, you can select a form for the patient, if the patient already has records for this form, then the form will be displayed for the patient with these records. If it's the first time to create this form for the patient, an empty form will be created for this patient, all you need to do is update these empty records for these fields.",
     github: 'https://github.com/Xiaoping-Li/patient_files_manager',
     youtube: '',
     live: 'https://patient-files-manager.herokuapp.com/',
   },
   {
-    img: ['', ''],
+    img: [
+      require('../images/shopping.png'), 
+      ''
+    ],
     title: 'My Todos',
     descShort: 'React Project',
     frontEnd: 'JavaScript, React, HTML, CSS',
     backEnd: '',
-    desc: '...',
+    desc: "This is the first project I did during the first week of my React study, and doesn't have backend. My_Todos is a small project with basic functions.",
     github: 'https://github.com/Xiaoping-Li/My-Todos',
     youtube: '',
     live: 'https://xiaoping-li.github.io/My-Todos/',
