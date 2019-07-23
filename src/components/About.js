69f92db65d3d468daa1e8474002958168a40f06a
Xiaoping-Li <narci2008@gmail.com>
2019-07-23 12:18:53 -0700

    Modify about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,11 +7,11 @@ function About() {
       <div className="about-desc">
         <p>The moment when I realized that if I could join such a great engineering team like the one that I worked with, I would be able to make a bigger impact for users who want to get things done and experience an amazing UI. I left the clinical job and dive head deeply into software engineering study. Having several months of intensive training behind me, now I am a full stack web developer.</p>
         <div className="about-desc-img">
-          <img src={require('../images/web-developer-2.jpg')} alt="images of web developer"/>
+          <img src={require('../images/developer.jpg')} alt="images of web developer"/>
         </div>
       </div>
 
-      <div className="about-paper-plane-right">
+      <div className="about-paper-plane">
         <img src={require('../images/paper_plane_right.png')} alt="paper-plane-right" />
       </div>
 
@@ -22,7 +22,7 @@ function About() {
         <p>After moving to United States, I found a job in the Clinical Department of a Bio-medical Device Company, and worked as Clinical Trial Coordinator for three years. In that position we processed tons of paperwork everyday and tracked everything in excel. As the company grew, we started to use Bio-medical platforms to automate tracking, and I got a chance to work closely with an engineering team to configure a platform for Clinical Trials. I was shocked by the high efficiency of the system and the user friendly interface. At that moment I wanted to become a web developer!</p>
       </div>
 
-      <div className="about-paper-plane-left">
+      <div className="about-paper-plane">
         <img src={require('../images/paper_plane_left.png')} alt="paper-plane-left" />
       </div>
 
@@ -33,7 +33,7 @@ function About() {
         </div>
       </div>
 
-      <div className="about-paper-plane-right">
+      <div className="about-paper-plane">
         <img src={require('../images/paper_plane_right.png')} alt="paper-plane-right" />
       </div>
 
