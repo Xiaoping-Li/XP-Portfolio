8d170552c78be05f8016870ac7c637fdcb6e885b
Xiaoping-Li <narci2008@gmail.com>
2019-07-25 15:31:59 -0700

    Basic layout done

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -3,63 +3,63 @@ export const projects = [
     img: [
       {
         url: require('../images/shopping.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: '',
       },
       {
         url: require('../images/pets-shopping1.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Welcome Screen with SIGN IN, SIGN UP and Autoplay pets\' photos Carousel.',
       },
       {
         url: require('../images/pets-shopping2.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'SIGN UP Screen with link to SIGN IN Screen. SIGN IN Screen with RESET PASSWORD link.',
       },
       {
         url: require('../images/pets-shopping3.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Provide Register Email, and a reset password link will be sent to this Email.',
       },
       {
         url: require('../images/pets-shopping4.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Open the Reset Password Link Email, click the link and navigate to RESET PASSWORD Screen in the App, and update password. Will navigate back to SIGN IN Screen automatically.',
       },
       {
         url: require('../images/pets-shopping5.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'This is what the Home Screen looks like, and could open Drawer Navigator by clicking the "Burger Icon" or by swiping the screen from left to right on iPhone.',
       },
       {
         url: require('../images/pets-shopping6.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Aquarium and Birds Screens by clicking corresponding Tabs on Home Screen.',
       },
       {
         url: require('../images/pets-shopping7.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Fluffy and Reptile Screens by clicking corresponding Tabs on Home Screen.',
       },
       {
         url: require('../images/pets-shopping8.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Profile Screen: show and modify personal profile.',
       },
       {
         url: require('../images/pets-shopping9.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Empty Shopping Cart, has a link to Home Screen.',
       },
       {
         url: require('../images/pets-shopping10.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Select a pet from pets\' cards list, will show a pets Carousel which is nested in a Modal, and will show the selected pet as active Carousel slide. Could swipe screen to play the Carousel. "Money Icon" navigate back to Shopping Cart; "Shopping Cart Icon" will add pet to Shopping Cart with quantity 1, no duplicate pets could be added to a specific shopping cart.',
       },
       {
         url: require('../images/pets-shopping11.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Shopping Cart with pets. Every time a pet was added to Shopping cart, quantity 1 was reserved for this specific order, and could modify order quantity, and the pet\'s storage quantity will be updated synchronously.',
       },
       {
         url: require('../images/pets-shopping12.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Shipping Address Screen and Order Summary Screen.',
       },
       {
         url: require('../images/pets-shopping13.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Payment Screen supported by Stripe.',
       },
       {
         url: require('../images/pets-shopping14.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'When the Payment is completed, the order\'s stats will be updated to SUCCESS, and then the order could be tracked on "Orders List".',
       }  
     ],
     id: 'pets-e-shopping',
@@ -76,51 +76,51 @@ export const projects = [
     img: [
       {
         url: require('../images/instagram.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: '',
       },
       {
         url: require('../images/instagram1.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Welcome Screen with SIGN IN and SIGN UP navigators.',
       },
       {
         url: require('../images/instagram2.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'UI for SIGN IN and SIGN UP Screens. On the bottom of SIGN UP Screen, \'Already have an account? Log In\' will navigate to SIGN IN',
       },
       {
         url: require('../images/instagram3.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'On the AddPost Screen, there are two ways to add photos: Select a photo from Album, or, Take a photo by Camera.',
       },
       {
         url: require('../images/instagram4.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'UI for Select a photo from Album (LEFT), or, Take a photo by Camera (RIGHT).',
       },
       {
         url: require('../images/instagram5.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'From the Home Screen, could see the latest posts from yourself and your friends. Could LIKE, COMMENT, and DOWNLOAD these photos, also could delete yourself\'s posts.',
       },
       {
         url: require('../images/instagram6.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Profile Screens, could show and edit personal information here.',
       },
       {
         url: require('../images/instagram7.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Search a user from Search Screen, if haven\'t connected yet, will show an \"Add Friend\" blue button, after CLICK, a FRIEND REQUEST will be sent to that user.',
       },
       {
         url: require('../images/instagram8.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'After you CLICK the \"Add Friend\" blue button, search this user again, now the FRIEND STATUS will change to "REQUESTED". If you recieved a FRIEND REQUEST, and search the sender\'s user name, you could see the FRIEND STATUS is PENDING, which means you have a pending FRIEND REQUEST that is waiting for you to process.',
       },
       {
         url: require('../images/instagram9.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'From the REQUESTS Tab on Profiles Screen, you could see all pending requests. If you choose ACCEPT, user will be added to your FRIENDS Tab.',
       },
       {
         url: require('../images/instagram10.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'If you search one of your friends, you could see that FRIEND STATUS has been updated to FRIENDS.',
       },
       {
         url: require('../images/instagram11.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'On the LIKES Screen, stores all the posts that you liked.',
       },
     ],
     id: 'instagram-clone',
@@ -137,39 +137,39 @@ export const projects = [
     img: [
       {
         url: require('../images/howdiditgo.jpg'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: '',
       },
       {
         url: require('../images/how-did-it-go1.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'This is the Welcome Page. Right up corner is the Dynamic Navigation Bar, after SIGN IN, more links will show up.',
       }, 
       {
         url: require('../images/how-did-it-go2.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'This belongs to Settings Page. From here you could edit your message template, which will be used when you text your customers.',
       },
       {
         url: require('../images/how-did-it-go3.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'This belongs to Settings Page. You could update your business Platform URL here, and show all the URLs on the list below.',
       },
       {
         url: require('../images/how-did-it-go4.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'This belongs to Settings Page. Password and Username could be updated here.',
       },
       {
         url: require('../images/how-did-it-go5.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'This belongs to Settings Page. Update Billing information: Business Name and Billing Address.',
       },
       {
         url: require('../images/how-did-it-go6.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'This belongs to Settings Page. Could Make a Payment here for the service, and was supported by Stripe.',
       },
       {
         url: require('../images/how-did-it-go7.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Send text message Invigations on this page and supported by TWILIO. Use custormers\' Name and Phone Number, send the text message (saved as template earlier), and invite them to leave reviews on the business platforms.',
       },
       {
         url: require('../images/how-did-it-go8.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'Check Invitations\' STATS here. Using BITLY to track business short URLs clicked and reviewed stats.',
       },
     ],
     id: 'how-did-it-go',
@@ -186,19 +186,19 @@ export const projects = [
     img: [
       {
         url: require('../images/patientFiles.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: '',
       },
       {
         url: require('../images/patient-files-manager.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: "This is the Patient Register Page. After filled in patient's information and clicked SAVE button, patient will be saved to the database, and patient's name will show up on the list below.",
       },
       {
         url: require('../images/patient-files-manager1.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'This is the Patient Information Page. When CLICK a patient from the list, will navigate to this page. From the FORM DROPDOWN LIST, could select specific Medical Form for the patient. If there are already data for this visit, the data will show up. ',
       },
       {
         url: require('../images/patient-files-manager2.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'If there are no data of the specific form for this visit, a blank form will be created.',
       },
     ],
     id: 'patient-files-manager',
@@ -206,7 +206,7 @@ export const projects = [
     descShort: 'First Personal Full-stack React Project',
     frontEnd: 'React, Redux, JavaScript, React-Bootstrap',
     backEnd: 'Express, Express-session, bcrypt, JWT, MySQL, Heroku',
-    desc: "I designed separate Forms table, Fields table, and Records table. Because the format and content of a medical form will change frequently per protocols, so this design will make it easier and cheaper to modify the forms, only need to insert rolls to Forms and Fields tables. When you select a form for a specific patient, if it's the first time to create this form, an empty form will be created, all you need to do is filling the form. Otherwise, the form will be displayed for the patient with these records.",
+    desc: "I designed separate Forms, Fields, and Records tables. Because the medical form will change frequently per protocols, this design will make it easier and cheaper to modify the forms tables. If it's the first time to create a specific form for patient visit, an empty form will be created, otherwise, the form will be displayed for the patient with these records.",
     github: 'https://github.com/Xiaoping-Li/patient_files_manager',
     youtube: '',
     live: 'https://patient-files-manager.herokuapp.com/',
@@ -215,11 +215,11 @@ export const projects = [
     img: [
       {
         url: require('../images/todo.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: '',
       },
       {
         url: require('../images/My Todos.png'),
-        desc: 'lskdfjeljrlejtlkejltj',
+        desc: 'This is my first React Project, which only has Frontend UI with basic features: Add Todo, Delete Todo, and Complete Check.',
       }
     ],
     id: 'my-todos',
