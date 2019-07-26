01f9bed09b034ec7d056e26c2ba6638c3adf2693
Xiaoping-Li <narci2008@gmail.com>
2019-07-26 09:56:18 -0700

    Modify url

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
       <ScrollToTop>
         <div className="App">
         <Navigation />
-        <Route path='/' exact component={Home} />
-        <Route path='/about' component={About} />
-        <Route path='/projects' component={Projects} />
+        <Route path='/xp-portfolio/' exact component={Home} />
+        <Route path='/xp-portfolio/about' component={About} />
+        <Route path='/xp-portfolio/projects' component={Projects} />
         <Footer />
       </div>
       </ScrollToTop>
