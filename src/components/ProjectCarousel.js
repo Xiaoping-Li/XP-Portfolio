8713cd8736b2c7270eeab65d18583c3b6593fe67
Xiaoping-Li <narci2008@gmail.com>
2019-08-07 17:32:01 -0700

    Project carousel

--- src/components/ProjectCarousel.js
+++ src/components/ProjectCarousel.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
 import '../SCSS/components/projectCarousel.scss';
-
+ 
 function ProjectCarousel({img}) {
   return (
     <Carousel
       interval={null}
+      indicators={false}
     >
       {img.map((image, idx) => {
         return (
           <Carousel.Item key={idx}>
-            <div className="slides-show-container">
-              <div className="glyphicon">{"<"}</div>
-
-              <div className="slides-show">
-                <img src={image.url} alt="photos of project" className="slides-show-img" />
-                <div className="slides-show-desc">{image.desc}</div>
-              </div>
-
-              <div className="glyphicon">{">"}</div>
+            <div className="slides-show">
+              <img src={image.url} alt="photos of project" />
+              <div className="slides-show-desc">{image.desc}</div>
             </div>   
           </Carousel.Item>
         );
