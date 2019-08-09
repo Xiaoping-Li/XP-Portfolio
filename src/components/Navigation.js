1fccd767f545e3bf25c12f060827ebe864142623
Xiaoping-Li <narci2008@gmail.com>
2019-08-08 17:22:36 -0700

    Small screen burger menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,26 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 import photo from '../images/Xiaoping Li.png';
 import '../SCSS/components/navBar.scss';
 
-function Navigation() {
-  return (
-    <nav className="nav-bar">
-      <div className="person-info">
-        <img src={photo} alt="avatar" className="thumbnail" />
-        <div className="title">
-          <div className="name">Xiaoping Li</div>
-          <div className="sub-title">Web | Mobile Development</div>
-        </div>
-      </div>
+class Navigation extends Component {
+  state = {
+    menuShow: false,
+  };
 
-      <div className="links">
-        <NavLink exact to='/' className="link" activeClassName="active-link">Home</NavLink>
-        <NavLink to='/about' className="link" activeClassName="active-link">About</NavLink>
-        <NavLink to='/projects' className="link" activeClassName="active-link">Projects</NavLink>
-      </div>
-    </nav> 
-  );
+  toggleMenuShow = () => this.setState({ menuShow: !this.state.menuShow })
+
+  render() {
+    return (
+      <nav className="nav-bar">
+        <div className="person-info">
+          <img src={photo} alt="avatar" className="thumbnail" />
+          <div className="title">
+            <div className="name">Xiaoping Li</div>
+            <div className="sub-title">Web | Mobile Development</div>
+          </div>
+        </div>
+  
+        <div className="links">
+          <NavLink exact to='/' className="link" activeClassName="active-link">Home</NavLink>
+          <NavLink to='/about' className="link" activeClassName="active-link">About</NavLink>
+          <NavLink to='/projects' className="link" activeClassName="active-link">Projects</NavLink>
+        </div>
+
+        <div className="burger-menu">
+          <FontAwesomeIcon 
+            icon={faBars} 
+            size="2x" 
+            onClick={this.toggleMenuShow} 
+            className={this.state.menuShow ? "menu-icon-hide" : "menu-icon-live"}
+          />
+          <div className={this.state.menuShow ? "menu-list-live" : "menu-list-hide"} >
+            <NavLink exact to='/' className="link" activeClassName="active-link" onClick={this.toggleMenuShow}>Home</NavLink>
+            <NavLink to='/about' className="link" activeClassName="active-link" onClick={this.toggleMenuShow}>About</NavLink>
+            <NavLink to='/projects' className="link" activeClassName="active-link" onClick={this.toggleMenuShow}>Projects</NavLink>
+          </div>
+        </div>
+      </nav> 
+    );
+  } 
 }
 
 export default Navigation;
