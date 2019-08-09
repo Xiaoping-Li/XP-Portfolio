f323144b9f8ad91e6078eb20735c7be1fbcc0618
Xiaoping-Li <narci2008@gmail.com>
2019-08-09 10:55:53 -0700

    About responsive style

--- src/components/About.js
+++ src/components/About.js
@@ -9,10 +9,12 @@ function About() {
       <ScrollToTopOnMount />
 
       <div className="about-desc">
-        <p className="p-left">The moment when I realized that if I could join such a great engineering team like the one that I worked with, 
-          I would be able to make a bigger impact for users who want to get things done and experience an amazing UI. I left the clinical job and dive head
-          deeply into software engineering study. Having several months of intensive training behind me, now I am a full stack web developer.
-        </p>
+        <div className="p-container left">
+          <p className="p-left">The moment when I realized that if I could join such a great engineering team like the one that I worked with, 
+            I would be able to make a bigger impact for users who want to get things done and experience an amazing UI. I left the clinical job and dive head
+            deeply into software engineering study. Having several months of intensive training behind me, now I am a full stack web developer.
+          </p>
+        </div>
         <div className="about-desc-img img-even">
           <img src={require('../images/developer.jpg')} alt="images of web developer"/>
         </div>
@@ -26,12 +28,14 @@ function About() {
         <div className="about-desc-img img-odd">
           <img src={require('../images/paperwork 1.jpg')} alt="images of a pile of files" />
         </div>
-        <p className="p-right">After moving to United States, I found a job in the Clinical Department of a Bio-medical Device Company, and worked as 
-          Clinical Trial Coordinator for three years. In that position we processed tons of paperwork everyday and tracked everything in excel. As the 
-          company grew, we started to use Bio-medical platforms to automate tracking, and I got a chance to work closely with an engineering team to 
-          configure a platform for Clinical Trials. I was shocked by the high efficiency of the system and the user friendly interface. At that moment I 
-          wanted to become a web developer!
-        </p>
+        <div className="p-container right">
+          <p className="p-right">After moving to United States, I found a job in the Clinical Department of a Bio-medical Device Company, and worked as 
+            Clinical Trial Coordinator for three years. In that position we processed tons of paperwork everyday and tracked everything in excel. As the 
+            company grew, we started to use Bio-medical platforms to automate tracking, and I got a chance to work closely with an engineering team to 
+            configure a platform for Clinical Trials. I was shocked by the high efficiency of the system and the user friendly interface. At that moment I 
+            wanted to become a web developer!
+          </p>
+        </div>
       </div>
 
       <div className="about-paper-plane plane-left">
@@ -39,9 +43,11 @@ function About() {
       </div>
 
       <div className="about-desc">
-        <p className="p-left">My love for Neuroscience brought me to University of Birmingham, UK after graduation from China. From there I got my second
-          Masters degree in Neuropsychology. I came to United States after my graduation and live in Bay Area since then.
-        </p>
+        <div className="p-container left">
+          <p className="p-left">My love for Neuroscience brought me to University of Birmingham, UK after graduation from China. From there I got my second
+            Masters degree in Neuropsychology. I came to United States after my graduation and live in Bay Area since then.
+          </p>
+        </div>
         <div className="about-desc-img img-even">
           <img src={require('../images/brain.jpg')} alt="images of brain" />
         </div>
@@ -55,9 +61,11 @@ function About() {
         <div className="about-desc-img img-odd">
           <img src={require('../images/DNA1.jpg')} alt="images of DNA" />
         </div>
-        <p className="p-right">I have a Masters degree in Biochemistry and Melocular Biology from Fudan University of China, and almost 10 years' 
-          experience in this field.
-        </p>
+        <div className="p-container right">
+          <p className="p-right">I have a Masters degree in Biochemistry and Melocular Biology from Fudan University of China, and almost 10 years' 
+            experience in this field.
+          </p>
+        </div> 
       </div>
     </div>
   );
