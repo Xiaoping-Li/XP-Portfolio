5a9047732865682cf56f2de4131e583bfbcefa2a
Xiaoping-Li <narci2008@gmail.com>
2019-10-14 14:57:44 -0700

    Remove LinkedIn

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,10 +16,7 @@ function Footer() {
           <FontAwesomeIcon icon={faGithubSquare} size="2x" inverse />
           <span className="item-name">GITHUB</span>
         </a>
-        <a className="list-item" href="https://www.linkedin.com/in/xiaoping-li-21405594/" target="_blank">
-          <FontAwesomeIcon icon={faLinkedin} size="2x" inverse />
-          <span className="item-name">LINKEDIN</span>
-        </a>
+        
         <a className="list-item" href="https://www.youtube.com/channel/UC8DxiP44q-zWFkVXDKVEbFg?view_as=subscriber" target="_blank">
           <FontAwesomeIcon icon={faYoutubeSquare} size="2x" inverse />
           <span className="item-name">YOUTUBE</span>
